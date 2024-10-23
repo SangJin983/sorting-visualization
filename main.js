ccef9869a30a65faae8d963f68068bd0cbe1f809
SangJin983 <lazistar983@gmail.com>
2024-10-23 22:37:08 +0900

    bar 그리기

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-console.log("hello");
+const arrayContainerElement = document.querySelector(".array-container");
 
-const hiBtnElement = document.querySelector(".hi-button");
-const containerElement = document.querySelector(".container");
+const initialArray = [30, 60, 90, 20, 50, 80, 10, 70, 40, 100];
 
-hiBtnElement.addEventListener("click", () => {
-  const textDiv = document.createElement("div");
-  textDiv.textContent = "hello, lezi";
-  containerElement.append(textDiv);
-});
+const renderArray = (array) => {
+  arrayContainerElement.innerHTML = ""; // array-container 초기화
+
+  // bar 생성하기.
+  array.forEach((value) => {
+    const barElement = document.createElement("div");
+    barElement.className = "array-bar";
+    barElement.style.height = `${value * 3}px`;
+    barElement.textContent = value;
+    arrayContainerElement.append(barElement);
+  });
+};
+
+renderArray(initialArray);
