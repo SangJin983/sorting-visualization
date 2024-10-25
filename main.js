da25fa9b79e9d602b3f185f355cc988028d52839
SangJin983 <lazistar983@gmail.com>
2024-10-26 05:15:01 +0900

    refactor: Renderer 클래스 작성

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
 import { generateRandomArray } from "./utils/generateRandomArray";
+import { ArrayRenderer, BarRenderer } from "./Renderer";
 
 const arrayContainerElement = document.querySelector(".array-container");
+const btnContainerElement = document.querySelector(".btn-container");
 const generateBtnElement = document.querySelector(".generate-btn");
 const sortingBtnElement = document.querySelector(".sorting-btn");
 
 // `배열 생성` 버튼 클릭시, 무작위 배열 생성 (10 ~ 100)
 generateBtnElement.addEventListener("click", () => {
   const initialArray = generateRandomArray(10, 10, 100); // size, min, max
-  renderArray(initialArray);
+  ArrayRenderer.render(initialArray, arrayContainerElement);
 });
 
 // `정렬 시작` 버튼 클릭시, 버블 정렬
 sortingBtnElement.addEventListener("click", async () => {
-  sortingBtnElement.disabled = true;
-  generateBtnElement.disabled = true;
+  setButtonDisabled(true, getButtons());
   await bubbleSort(getCurrentArray());
-  sortingBtnElement.disabled = false;
-  generateBtnElement.disabled = false;
+  setButtonDisabled(false, getButtons());
 });
 
-// 배열 시각화
-const renderArray = (array) => {
-  arrayContainerElement.innerHTML = ""; // array-container 초기화
-
-  // bar 생성하기
-  array.forEach((value) => {
-    const barElement = document.createElement("div");
-    barElement.className = "array-bar";
-    barElement.style.height = `${value * 3}px`; // bar 높이 설정
-    barElement.textContent = value;
-    arrayContainerElement.append(barElement);
-  });
+const setButtonDisabled = (isDisabled, buttons) => {
+  buttons.forEach((btn) => (btn.disabled = isDisabled));
 };
 
-// 현재 배열 확인
+const getButtons = () => {
+  return Array.from(btnContainerElement.children);
+};
+
 const getCurrentArray = () => {
   const barsElement = document.querySelectorAll(".array-bar");
   const currentArray = Array.from(barsElement).map((bar) =>
@@ -49,26 +42,23 @@ const bubbleSort = async (array) => {
 
   for (let i = 0; i < result.length; i += 1) {
     for (let j = 0; j < result.length - i - 1; j += 1) {
-      // 비교 중인 bar 색깔 변경
-      barsElement[j].style.backgroundColor = "red";
-      barsElement[j + 1].style.backgroundColor = "red";
+      // 비교 중인 bar 색깔 설정
+      BarRenderer.setBackgroundColor("red", barsElement[j], barsElement[j + 1]);
 
+      // 1초 대기 후 진행
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       if (result[j] > result[j + 1]) {
         // 스왑
         [result[j], result[j + 1]] = [result[j + 1], result[j]];
 
-        // 스왑에 해당하는 bar의 높이와 값을 변경
-        barsElement[j].textContent = result[j];
-        barsElement[j].style.height = `${result[j] * 3}px`;
-        barsElement[j + 1].textContent = result[j + 1];
-        barsElement[j + 1].style.height = `${result[j + 1] * 3}px`;
+        // 랜더 중인 바의 상태를 업데이트
+        BarRenderer.updateBar(result[j], barsElement[j]);
+        BarRenderer.updateBar(result[j + 1], barsElement[j + 1]);
       }
 
       // 비교 중인 bar의 색깔 리셋
-      barsElement[j].style.backgroundColor = "";
-      barsElement[j + 1].style.backgroundColor = "";
+      BarRenderer.setBackgroundColor("", barsElement[j], barsElement[j + 1]);
     }
   }
 };
