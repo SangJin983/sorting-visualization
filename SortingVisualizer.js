e978c2657976b52fe6633896ecf34e173a37623c
SangJin983 <lazistar983@gmail.com>
2024-10-29 06:14:57 +0900

    feat: 알고리즘 선택 기능 추가

--- SortingVisualizer.js
+++ SortingVisualizer.js
@@ -17,17 +17,21 @@ export class BubbleSortVisualizer extends SortingVisualizer {
   }
 
   async visualize() {
-    const result = [...this.array]; 
+    const result = [...this.array];
     const barsElement = this.bars;
 
     // 버블 정렬 시각화 로직
     for (let i = 0; i < result.length; i += 1) {
       for (let j = 0; j < result.length - i - 1; j += 1) {
         // 비교 중인 바 색깔 설정
-        BarRenderer.setBackgroundColor("red", barsElement[j], barsElement[j + 1]);
+        BarRenderer.setBackgroundColor(
+          "red",
+          barsElement[j],
+          barsElement[j + 1]
+        );
 
         // 1초 대기 후 진행
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         if (result[j] > result[j + 1]) {
           // 스왑
@@ -41,6 +45,48 @@ export class BubbleSortVisualizer extends SortingVisualizer {
         // 비교 중인 바의 색깔 리셋
         BarRenderer.setBackgroundColor("", barsElement[j], barsElement[j + 1]);
       }
+    }
+  }
+}
+
+export class SelectionSortVisualizer extends SortingVisualizer {
+  constructor(array, bars) {
+    super(array);
+    this.bars = bars;
+  }
+
+  async visualize() {
+    const result = [...this.array];
+    const barsElement = this.bars;
+    let indexMin;
+
+    for (let i = 0; i < result.length - 1; i += 1) {
+      indexMin = i;
+      BarRenderer.setBackgroundColor("red", barsElement[indexMin]); // bar1 선택
+      let lastIndex;
+      for (let j = i + 1; j < result.length; j += 1) {
+        BarRenderer.setBackgroundColor("red", barsElement[j]); // bar2 선택
+
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 대기
+
+        // 크기 비교 후, 더 작은 bar 선택
+        if (result[j] < result[indexMin]) {
+          BarRenderer.setBackgroundColor("", barsElement[indexMin]); // bar1 취소
+          indexMin = j;
+        } else {
+          BarRenderer.setBackgroundColor("", barsElement[j]); // bar2 취소
+        }
+        
+        // 마지막 인덱스 저장
+        lastIndex = j;
+      }
+      // 비교 bar 리셋
+      BarRenderer.setBackgroundColor("", barsElement[lastIndex]);
+      BarRenderer.setBackgroundColor("", barsElement[indexMin]);
+      // 스왑
+      [result[i], result[indexMin]] = [result[indexMin], result[i]];
+      BarRenderer.updateBar(result[i], barsElement[i]);
+      BarRenderer.updateBar(result[indexMin], barsElement[indexMin]);
     }
   }
 }
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import { generateRandomArray } from "./utils/generateRandomArray";
 import { ArrayRenderer } from "./Renderer";
-import { BubbleSortVisualizer } from "./SortingVisualizer";
+import {
+  BubbleSortVisualizer,
+  SelectionSortVisualizer,
+} from "./SortingVisualizer";
 
+const selectContainerElement = document.querySelector(".select-container");
 const arrayContainerElement = document.querySelector(".array-container");
 const btnContainerElement = document.querySelector(".btn-container");
 const generateBtnElement = document.querySelector(".generate-btn");
@@ -9,20 +13,59 @@ const sortingBtnElement = document.querySelector(".sorting-btn");
 
 const buttons = Array.from(btnContainerElement.children);
 
+// 알고리즘을 선택
+let selectedSortAlgorithm;
+
+selectContainerElement.addEventListener("change", (event) => {
+  arrayContainerElement.innerHTML = "" // 배열 컨테이너 초기화
+  selectedSortAlgorithm = createSortVisualizer(event.target.value);
+});
+
+const createSortVisualizer = (value) => {
+  if (value === "bubble") {
+    return (array, bars) => {
+      return new BubbleSortVisualizer(array, bars);
+    };
+  }
+
+  if (value === "selection") {
+    return (array, bars) => {
+      return new SelectionSortVisualizer(array, bars);
+    };
+  }
+
+  return null;
+};
+
 // `배열 생성` 버튼 클릭시, 무작위 배열 생성 (10 ~ 100)
 generateBtnElement.addEventListener("click", () => {
+  if (!selectedSortAlgorithm) {
+    console.error("정렬 알고리즘이 선택되지 않았습니다.");
+    return;
+  }
+  
   const initialArray = generateRandomArray(10, 10, 100); // size, min, max
   ArrayRenderer.render(initialArray, arrayContainerElement);
 });
 
-// `정렬 시작` 버튼 클릭시, 버블 정렬
+// `정렬 시작` 버튼 클릭시, 선택한 정렬 실행
 sortingBtnElement.addEventListener("click", async () => {
+  if (!selectedSortAlgorithm) {
+    console.error("정렬 알고리즘이 선택되지 않았습니다.");
+    return;
+  }
+
+  if (getBars().length === 0) {
+    console.error("배열을 생성하세요.");
+    return;
+  }
+
   const bars = getBars();
   const array = getArrayFromItems(bars);
-  const bubbleSortVisualizer = new BubbleSortVisualizer(array, bars);
-  
+  const sortVisualizer = selectedSortAlgorithm(array, bars);
+
   setButtonDisabled(true, buttons);
-  await bubbleSortVisualizer.visualize();
+  await sortVisualizer.visualize();
   setButtonDisabled(false, buttons);
 });
 
